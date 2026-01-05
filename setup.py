d6387118f53aada33d50514599003f6f6e005035
RICHARD-OBENG <richardkwabenaobeng17@gmail.com>
2026-01-05 19:59:13 +0000

    chore: improve package description in [setup.py]

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Multi-Language-Customer-Support-Chatbot"
+AUTHOR_USER_NAME = "RICHARD-OBENG"
+SRC_REPO = "Multi-Language Customer Support Chatbot"
+AUTHOR_EMAIL = "richardkwabenaobeng17@gmail.com"
+
+setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Multi-language AI customer support chatbot providing intent detection, multilingual response generation, and retrieval capabilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
